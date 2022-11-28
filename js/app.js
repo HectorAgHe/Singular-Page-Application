3838d06aa53f1934f39cae5936c82d5dda0630ab
HectorAgHe <aguilarhector440@gmail.com>
2022-11-28 11:49:53 -0600

    Navegacion entre clases

--- js/app.js
+++ js/app.js
@@ -1,24 +1,46 @@
 const mainContent = document.querySelector('#container');
-const navbar = document.querySelector('./navbar');
+const navbar = document.querySelector('.navbar');
 
-const loadData = e =>{
+
+const navAction = e =>{
     e.preventDefault();
-    if(e.target.clasList.contains('')|| e.target.clasList.contains('')){
+    if(e.target.classList.contains('navbar-item')
+    || e.target.classList.contains('button')){
+        const opcion = e.target.innerHTML.trim();
 
+
+        switch(opcion){
+            case 'Home':
+                loadData('../pages/home.html');
+                break;
+            case 'Tabla':
+                loadData('../pages/table.html');
+                break;
+            case 'Log in':
+                loadData('../pages/login.html');
+                break;
+        }
     }
 }
+const loadData = (pageUrl)=>{ 
+        fetch(pageUrl)
+        .then(page =>{
+            return page.text();
+            
+        })
+        .then(contentHtml => {
+            if(mainContent.firstChild){
+                mainContent.removeChild(mainContent.firstChild);
+            }
+            const div = document.createElement('div');
+            div.innerHTML = contentHtml;
+            mainContent.appendChild(div);
+        })
+}
+
 window.addEventListener('DOMContentLoaded', () =>{
-    fetch('..pages/home.html')
-    .then(page =>{
-        //const div = document.createElement('div);
-        return page.text();
-        
-    })
-    .then(contentHtml => {
-        const div = document.createElement('div');
-        console.log(contentHtml);
-        div.innerHTML = contentHtml;
-        mainContent.appendChild(div);
-    });
-    navbar.addEventListener('click', loadData);
-});
+
+    loadData('../pages/home.html');
+
+    navbar.addEventListener('click', navAction);
+})
